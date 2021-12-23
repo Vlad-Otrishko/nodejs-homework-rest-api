a5edaa5f160467ca848adccf4566d07d1e177bb7
Vlad-Otrishko <reactor.doe0@gmail.com>
2021-12-24 00:51:13 +0200

    creates application for performing CRUD operations with MongoDB data base

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 const express = require('express')
 const router = express.Router()
 const { NotFound, BadRequest } = require('http-errors')
@@ -5,13 +6,20 @@ const Joi = require('joi')
 const joiSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
-  phone: Joi.string().required()
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 })
-const contactsOperations = require('../../model/contacts')
+const patchJoiSchema = Joi.object({
+  name: Joi.string().optional(),
+  email: Joi.string().optional(),
+  phone: Joi.string().optional(),
+  favorite: Joi.boolean(),
+})
+const { Contact } = require('../../model')
 
 router.get('/', async (req, res, next) => {
   try {
-    const contacts = await contactsOperations.listContacts()
+    const contacts = await Contact.find()
     res.json(contacts)
   } catch (error) {
     next(error)
@@ -20,12 +28,15 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   const { id } = req.params
   try {
-    const product = await contactsOperations.getContactById(id)
+    const product = await Contact.findById(id)
     if (!product) {
       throw new NotFound()
     }
     res.json(product)
   } catch (error) {
+    if (error.message.includes('Cast to ObjectId failed')) {
+      error.status = 404
+    }
     next(error)
   }
 })
@@ -36,9 +47,17 @@ router.post('/', async (req, res, next) => {
     if (error) {
       throw new BadRequest(error.message)
     }
-    const newContact = await contactsOperations.addContact(req.body)
+    if (!Object.keys(req.body).includes('favorite')) {
+      req.body.favorite = false
+    }
+
+    const newContact = await Contact.create(req.body)
     res.status(201).json(newContact)
   } catch (error) {
+    if (error.message.includes('validation failed')) {
+      error.status = 400
+    }
+
     next(error)
   }
 })
@@ -50,10 +69,7 @@ router.put('/:id', async (req, res, next) => {
       throw new BadRequest(error.message)
     }
     const { id } = req.params
-    const updateContact = await contactsOperations.updateContact({
-      id,
-      ...req.body
-    })
+    const updateContact = await Contact.findByIdAndUpdate(id, req.body, { new: true })
     if (!updateContact) {
       throw new NotFound()
     }
@@ -62,11 +78,31 @@ router.put('/:id', async (req, res, next) => {
     next(error)
   }
 })
+router.patch('/:contactId/favorite', async (req, res, next) => {
+  try {
+    const { error } = patchJoiSchema.validate(req.body)
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+    const { contactId } = req.params
+    if (Object.keys(req.body).length === 0) {
+      throw new Error('missing field favorite')
+    }
+    const updated = await updateStatusContact(contactId, req.body)
+    res.json(updated)
+  } catch (error) {
+    if (error.message.includes('missing field favorite')) {
+      error.status = 400
+    }
+
+    next(error)
+  }
+})
 
 router.delete('/:id', async (req, res, next) => {
   try {
     const { id } = req.params
-    const deleteContact = await contactsOperations.removeContact(id)
+    const deleteContact = await Contact.findByIdAndRemove(id)
     if (!deleteContact) {
       throw new NotFound()
     }
@@ -75,4 +111,13 @@ router.delete('/:id', async (req, res, next) => {
     next(error)
   }
 })
+
+async function updateStatusContact (contactId, body) {
+  const updateContact = await Contact.findByIdAndUpdate(contactId, body, { new: true })
+  if (!updateContact) {
+    throw new NotFound()
+  }
+  return updateContact
+}
+
 module.exports = router
