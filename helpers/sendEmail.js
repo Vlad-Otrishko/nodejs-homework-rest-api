7c9f39619ecfb0956ca80105a6a5dd1e1fe58121
Vlad-Otrishko <reactor.doe0@gmail.com>
2022-01-30 23:06:35 +0200

    bugfix: removed personal email from code

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,14 @@
 const sgMail = require('@sendgrid/mail')
 require('dotenv').config()
 
-const { SENDGRID_API_KEY } = process.env
+const { SENDGRID_API_KEY, MY_MAIL } = process.env
 
 sgMail.setApiKey(SENDGRID_API_KEY)
 
 const sendEmail = async (data) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    const email = { ...data, from: 'reactor.doe0@gmail.com' }
+    const email = { ...data, from: MY_MAIL }
     await sgMail.send(email)
     return true
   } catch (error) {
